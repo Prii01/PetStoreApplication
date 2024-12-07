8947686d6f8320fd7f3d152901f3152a042c48af
Priyanka Pujar <110797415+Prii01@users.noreply.github.com>
2024-12-07 12:13:16 +0530

    Create FrameworkConstants.java

---
+++ com.utility/FrameworkConstants.java
@@ -0,0 +1,13 @@
+package com.utility;
+
+public interface FrameworkConstants {
+	
+	String PROPERTIES_PATH="./src/test/resources/PropertiesFiles/CommonTestData.properties";
+	String EXCEL_PATH="./src/test/resources/ExcelFiles/TestCaseData.xlsx";
+	String REPORTS_PATH="./Reports/";
+	String SCREENSHOT_PATH="./Screenshot/";
+	
+	
+	String CHROME_EXE_PATH="C:\\\\\\\\\\\\\\\\Users\\\\\\\\\\\\\\\\91701\\\\\\\\\\\\\\\\Downloads\\\\\\\\\\\\\\\\chrome-win64\\\\\\\\\\\\\\\\chrome-win64\\\\\\\\\\\\\\\\chrome.exe";
+
+}
